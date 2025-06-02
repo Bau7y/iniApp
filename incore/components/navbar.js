8cce43e24c32d55a5d47bda57c528aed5672385b
Sebas <sebasrojash08@gmail.com>
2025-06-01 21:19:31 -0600

    feat: notificationBell navbar

--- incore/components/navbar.js
+++ incore/components/navbar.js
@@ -30,10 +30,20 @@ const Navbar = () => {
                             <div className="flex space-x-2">
                                 <a href="#" className="rounded-md bg-gray-900 px-2 py-2 text-sm font-medium text-white" aria-current="page">Home</a>
                                 <a href="#" className="rounded-md bg-gray-900 px-2 py-2 text-sm font-medium text-white" aria-current="page">Fragancias</a>
-                                <a href="#" className="rounded-md bg-gray-900 px-2 py-2 text-sm font-medium text-white" aria-current="page">Sneakers</a>
+                                {/* <a href="#" className="rounded-md bg-gray-900 px-2 py-2 text-sm font-medium text-white" aria-current="page">Sneakers</a> */}
                                 <a href="#" className="rounded-md bg-gray-900 px-2 py-2 text-sm font-medium text-white" aria-current="page">Asistencia</a>
                             </div>
                         </div>
+                    </div>
+                    <div className="absolute inset-y-0 right-0 flex items-center pr-2 sm:static sm:inset-auto sm:ml-6 sm:pr-0">
+                        <button type="button" className="relative rounded-full bg-gray-800 p-1 text-gray-400 hover:text-white focus:ring-2 focus:ring-white focus:ring-offset-2 focus:ring-offset-gray-800 focus:outline-hidden">
+                            <span className="absolute -inset-1.5"></span>
+                            <span className="sr-only">Ver notificaciones</span>
+                            <svg className="size-6" fill="none" viewBox="0 0 24 24" strokeWidth="1.5" stroke="currentColor" aria-hidden="true" data-slot="icon">
+                                <path strokeLinecap="round" strokeLinejoin="round" d="M14.857 17.082a23.848 23.848 0 0 0 5.454-1.31A8.967 8.967 0 0 1 18 9.75V9A6 6 0 0 0 6 9v.75a8.967 8.967 0 0 1-2.312 6.022c1.733.64 3.56 1.085 5.455 1.31m5.714 0a24.255 24.255 0 0 1-5.714 0m5.714 0a3 3 0 1 1-5.714 0" />
+                            </svg>
+
+                        </button>
                     </div>
                 </div>
             </div>
