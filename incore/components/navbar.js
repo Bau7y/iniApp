bed4081f67f460ea79a7992ae686c57dd0c1e678
Sebas <sebasrojash08@gmail.com>
2025-06-02 08:25:57 -0600

    feat: userIcon navbar

--- incore/components/navbar.js
+++ incore/components/navbar.js
@@ -42,8 +42,20 @@ const Navbar = () => {
                             <svg className="size-6" fill="none" viewBox="0 0 24 24" strokeWidth="1.5" stroke="currentColor" aria-hidden="true" data-slot="icon">
                                 <path strokeLinecap="round" strokeLinejoin="round" d="M14.857 17.082a23.848 23.848 0 0 0 5.454-1.31A8.967 8.967 0 0 1 18 9.75V9A6 6 0 0 0 6 9v.75a8.967 8.967 0 0 1-2.312 6.022c1.733.64 3.56 1.085 5.455 1.31m5.714 0a24.255 24.255 0 0 1-5.714 0m5.714 0a3 3 0 1 1-5.714 0" />
                             </svg>
-
                         </button>
+                        <div className="ml-3 relative">
+                            <div>
+                                <button className="relative flex rounded-full bg-gray-800 text-sm focus:ring-2 focus:ring-white">
+                                    <span className="absolute -inset-1.5"></span>
+                                    <span className="sr-only">Open user menu</span>
+                                    <Image
+                                    src="public\images\usuario-desconocido.svg"
+                                    alt="usuario no registrado"
+                                    width={30}
+                                    height={10}/>
+                                </button>
+                            </div>
+                        </div>
                     </div>
                 </div>
             </div>
